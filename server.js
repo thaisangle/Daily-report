5d21266cfb7af3cc34bdc38a4253273be60de8d2
Your Name <thaisang208@gmail.com>
2020-08-15 13:44:13 +0700

    Fix cors

--- server.js
+++ server.js
@@ -11,23 +11,24 @@ const initMongo = require("./config/mongo")
 require('dotenv-safe').config()
 
 //Setup COSR
-var allowedOrigins = ['http://localhost:3000',
-                      'https://daily-report.vercel.app',
-                      'https://daily-report-git-devhuy.stdiohue.vercel.app'
-                    ];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+// var allowedOrigins = ['http://localhost:3000',
+//                       'https://daily-report.vercel.app',
+//                       'https://daily-report-git-devhuy.stdiohue.vercel.app'
+//                     ];
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors())
 // End Setup
 
 // Setup express server port from ENV, default: 3000
